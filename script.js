25c59dc5012f1d6d4f38ae1a4116f377af7dbb6e
Nageshvara <nagaesvarab@gmail.com>
2025-03-06 09:03:04 +0530

    Added to check whether the same bookname exists under the author name or not

--- script.js
+++ script.js
@@ -11,34 +11,51 @@ const firebaseConfig = {
     appId: "1:750673335452:web:64a14da9e1afa5c5514959"
 };
 
-        const app = initializeApp(firebaseConfig);
-        const db = getDatabase();
+const app = initializeApp(firebaseConfig);
+const db = getDatabase();
 
-        function insertData() {
-            const authorRef = ref(db, "Authors/" + Author.value + "/Books");
-            set(ref(db, "Authors/" + Author.value + "/AuthorDetails"), {
-                Author: Author.value,
-                AuthorEmail: AuthorEmail.value
+function insertData() {
+    const authorRef = ref(db, "Authors/" + Author.value + "/Books");
+    get(authorRef).then((snapshot) => {
+        if (snapshot.exists()) {
+            let bookExists = false;
+            snapshot.forEach((childSnapshot) => {
+                if (childSnapshot.val().BookName === BookName.value) {
+                    bookExists = true;
+                }
             });
 
-            const newBookRef = push(authorRef);
-            set(newBookRef, {
-                BookName: BookName.value,
-                Publisher: Publisher.value,
-                Description: Description.value,
-                Price: Price.value
-            }).then(() => {
-                alert("Data is Entered");
-            }).catch((error) => {
-                alert("Unsuccessful: " + error);
-            });
-        }
-
-        document.getElementById("bookForm").addEventListener("submit", function (event) {
-            event.preventDefault();
-            if (this.checkValidity()) {
-                insertData();
-            } else {
-                this.classList.add("was-validated");
+            if (bookExists) {
+                alert("This book name already exists under this author.");
+                return;
             }
+        }
+        set(ref(db, "Authors/" + Author.value + "/AuthorDetails"), {
+            Author: Author.value,
+            AuthorEmail: AuthorEmail.value
         });
+
+        const newBookRef = push(authorRef);
+        set(newBookRef, {
+            BookName: BookName.value,
+            Publisher: Publisher.value,
+            Description: Description.value,
+            Price: Price.value
+        }).then(() => {
+            alert("Data is Entered");
+        }).catch((error) => {
+            alert("Unsuccessful: " + error);
+        });
+    }).catch((error) => {
+        alert("Error checking book existence: " + error);
+    });
+}
+
+document.getElementById("bookForm").addEventListener("submit", function (event) {
+    event.preventDefault();
+    if (this.checkValidity()) {
+        insertData();
+    } else {
+        this.classList.add("was-validated");
+    }
+});
